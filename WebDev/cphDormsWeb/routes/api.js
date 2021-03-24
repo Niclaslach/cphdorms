5324d4841c0020c8b8a94d8b3d933422e8f276c0
Niclas <niclas@robs-world.de>
2021-03-24 21:44:11 +0100

    Further cleaning

--- WebDev/cphDormsWeb/routes/api.js
+++ WebDev/cphDormsWeb/routes/api.js
@@ -4,21 +4,5 @@ const router = express.Router()
 
 /*  This is a sample API route. */
 
-router.get('/:resource', (req, res) => {
-  res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    query: req.query // from the url query string
-  })
-})
-
-router.get('/:resource/:id', (req, res) => {
-  res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    id: req.params.id,
-    query: req.query // from the url query string
-  })
-})
 
 module.exports = router
