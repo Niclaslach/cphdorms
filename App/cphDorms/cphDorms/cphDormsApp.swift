941e5599c9f3a5ba25bfbf66358f2b1a1ea2274f
Niclas <niclas@robs-world.de>
2021-04-23 20:02:47 +0200

    Create App

---
+++ App/cphDorms/cphDorms/cphDormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cphDormsApp.swift
+//  cphDorms
+//
+//  Created by Niclas Lach on 23.04.21.
+//
+
+import SwiftUI
+
+@main
+struct cphDormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
